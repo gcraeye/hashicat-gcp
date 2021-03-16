053242369f47821e545180f46446917facb6c110
gcraeye <root@p-kxclz8rylbnq-workstation.c.instruqt-prod.internal>
2021-03-16 15:40:09 +0000

    added the first label

--- main.tf
+++ main.tf
@@ -39,8 +39,7 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  department   = "develops"
-
+  
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts"
@@ -55,6 +54,7 @@ resource "google_compute_instance" "hashicat" {
 
   metadata = {
     ssh-keys = "ubuntu:${chomp(tls_private_key.ssh-key.public_key_openssh)} terraform"
+    department = "develops"
   }
 
   tags = ["http-server"]
